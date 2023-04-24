d1a5c9a02f4986df377ecba13e73996b1cf07c98
russellhz <horowitz.russell@gmail.com>
2023-04-24 11:49:00 +0200

    Ensure fuel standard always equal or below UCD SSP scenario

--- input/gcamdata/R/zchunk_L354.FuelStandard.R
+++ input/gcamdata/R/zchunk_L354.FuelStandard.R
@@ -15,7 +15,8 @@
 #' @author RLH April 2023
 module_policy_L354.FuelStandards <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
-    return(c(FILE = "policy/A_FuelStandards"
+    return(c(FILE = "policy/A_FuelStandards",
+             "L254.StubTranTechCoef"
     ))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L354.FuelStandards"))
@@ -25,17 +26,26 @@ module_policy_L354.FuelStandards <- function(command, ...) {
 
     # Load required inputs
     A_FuelStandards <- get_data(all_data, "policy/A_FuelStandards")
+    L254.StubTranTechCoef <- get_data(all_data, "L254.StubTranTechCoef")
 
     # Convert to long
     L354.FuelStandards <- A_FuelStandards %>%
       gather_years(value_col = "coefficient") %>%
-      mutate(market.name = region)
+      mutate(market.name = region) %>%
+      # Join in default GCAM coefficients and select the minimum coefficient
+      left_join_error_no_match(L254.StubTranTechCoef,
+                               by = c("region", "supplysector", "tranSubsector", "stub.technology",
+                                      "minicam.energy.input", "year",  "market.name",
+                                      "SSP_sce" = "sce")) %>%
+      mutate(coefficient = pmin(coefficient.x, coefficient.y)) %>%
+      select(xml, LEVEL2_DATA_NAMES[["StubTranTechCoef"]])
 
     # Produce outputs
     L354.FuelStandards %>%
       add_title("Policy fuel standards", overwrite = T) %>%
       add_units("BTU/vkm") %>%
-      add_precursors("policy/A_FuelStandards") ->
+      add_precursors("policy/A_FuelStandards",
+                     "L254.StubTranTechCoef") ->
       L354.FuelStandards
 
     return_data(L354.FuelStandards)
