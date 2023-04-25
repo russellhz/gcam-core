6a65f7e843eec08f4d38debb39dd979d1a1a400e
russellhz <horowitz.russell@gmail.com>
2023-04-25 13:18:06 +0200

    Coefficient and efficiency policies

--- input/gcamdata/R/zchunk_batch_policy_CoefEff.R
+++ input/gcamdata/R/zchunk_batch_policy_CoefEff.R
@@ -1,6 +1,6 @@
 # Copyright 2019 Battelle Memorial Institute; see the LICENSE file.
 
-#' module_policy_FuelStandards_xml
+#' module_policy_CoefEff_xml
 #'
 #' Construct XML data structure for \code{aeei.xml}.
 #'
@@ -8,12 +8,13 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{policy_FuelStandards.xml}.
-module_policy_FuelStandards_xml <- function(command, ...) {
-  all_xml_names <- get_xml_names("policy/A_FuelStandards.csv", "policy_FuelStandards.xml")
+#' the generated outputs: \code{policy_CoefEff.xml}.
+module_policy_CoefEff_xml <- function(command, ...) {
+  all_xml_names <- get_xml_names("policy/A_Coef_Eff.csv", "policy_CoefEff.xml")
 
   if(command == driver.DECLARE_INPUTS) {
-    return(c("L354.FuelStandards"))
+    return(c("L304.StubTechCoef",
+             "L304.StubTechEff"))
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(all_xml_names)
   } else if(command == driver.MAKE) {
@@ -21,19 +22,27 @@ module_policy_FuelStandards_xml <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
-    L354.FuelStandards <- get_data(all_data, "L354.FuelStandards")
+    L304.StubTechCoef <- get_data(all_data, "L304.StubTechCoef")
+    L304.StubTechEff <- get_data(all_data, "L304.StubTechEff")
+
     # ===================================================
 
     # Produce outputs
     for (xml_name in all_xml_names){
-      L354.FuelStandards_tmp <- L354.FuelStandards %>%
+      L304.StubTechCoef_tmp <- L304.StubTechCoef %>%
+        filter(xml == xml_name) %>%
+        select(-xml)
+
+      L304.StubTechEff_tmp <- L304.StubTechEff %>%
         filter(xml == xml_name) %>%
         select(-xml)
 
       assign(xml_name,
              create_xml(xml_name) %>%
-               add_xml_data(L354.FuelStandards_tmp, "StubTranTechCoef") %>%
-               add_precursors("L354.FuelStandards")
+               add_xml_data(L304.StubTechCoef_tmp, "StubTechCoef") %>%
+               add_xml_data(L304.StubTechEff_tmp, "StubTechEff") %>%
+               add_precursors("L304.StubTechCoef",
+                              "L304.StubTechEff")
       )
     }
 
@@ -44,7 +53,7 @@ module_policy_FuelStandards_xml <- function(command, ...) {
     }
     return_multiple_xmls(list_of_xmls, all_xml_names)
 
-      } else {
+  } else {
     stop("Unknown command")
   }
 }
