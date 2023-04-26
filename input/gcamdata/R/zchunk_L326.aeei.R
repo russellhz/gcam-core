e392fc8d617074fdb800336513c8cfce93dcadb1
russellhz <horowitz.russell@gmail.com>
2023-04-26 15:49:11 +0200

    aeei xml naming

--- input/gcamdata/R/zchunk_L326.aeei.R
+++ input/gcamdata/R/zchunk_L326.aeei.R
@@ -19,29 +19,29 @@ module_policy_L326.aeei <- function(command, ...) {
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L326.aeei"))
   } else if(command == driver.MAKE) {
-    
+
     all_data <- list(...)[[1]]
-    
+
     # Load required inputs
     A_aeei <- get_data(all_data, "policy/A_aeei")
-    
+
     # Convert to long format and interpolate any missing years
-    L326.aeei <- A_aeei %>% 
-      gather_years() %>% 
+    L326.aeei <- A_aeei %>%
+      gather_years() %>%
       # Interpolates between min and max years in A_aeei
-      complete(nesting(region, energy.final.demand), year = seq(min(year), max(year), 5)) %>% 
-      group_by(region, energy.final.demand) %>%
-      mutate(value = approx_fun(year, value)) %>% 
-      ungroup %>% 
+      complete(nesting(xml, region, energy.final.demand), year = seq(min(year), max(year), 5)) %>%
+      group_by(xml, region, energy.final.demand) %>%
+      mutate(value = approx_fun(year, value)) %>%
+      ungroup %>%
       rename(aeei = value)
-    
+
     # Produce outputs
     L326.aeei %>%
       add_title("AEEI (autonomous energy efficiency improvement index) parameters by region and sector", overwrite = T) %>%
       add_units("Unitless") %>%
       add_precursors("policy/A_aeei") ->
       L326.aeei
-    
+
     return_data(L326.aeei)
   } else {
     stop("Unknown command")
--- input/gcamdata/R/zchunk_batch_policy_aeei.R
+++ input/gcamdata/R/zchunk_batch_policy_aeei.R
@@ -10,25 +10,44 @@
 #' a vector of output names, or (if \code{command} is "MAKE") all
 #' the generated outputs: \code{aeei.xml}.
 module_policy_aeei_xml <- function(command, ...) {
+  all_xml_names <- get_xml_names("policy/A_aeei.csv", "policy_aeei.xml")
+
   if(command == driver.DECLARE_INPUTS) {
     return(c("L326.aeei"))
   } else if(command == driver.DECLARE_OUTPUTS) {
-    return(c(XML = "policy_aeei.xml"))
+    return(all_xml_names)
   } else if(command == driver.MAKE) {
-    
+
     all_data <- list(...)[[1]]
-    
+
     # Load required inputs
     L326.aeei <- get_data(all_data, "L326.aeei")
     # ===================================================
-    
+
     # Produce outputs
     create_xml("policy_aeei.xml") %>%
       add_xml_data(L326.aeei, "aeei") %>%
       add_precursors("L326.aeei") ->
       policy_aeei.xml
-    
-    return_data(policy_aeei.xml)
+
+    for (xml_name in all_xml_names){
+      L326.aeei_tmp <- L326.aeei %>%
+        filter(xml == xml_name) %>%
+        select(-xml)
+      # Produce output
+      assign(xml_name,
+             create_xml(xml_name) %>%
+               add_xml_data(L326.aeei_tmp, "aeei") %>%
+               add_precursors("L326.aeei"))
+
+    }
+
+    # Need this for loop because having issues with lapply(all_xml_names, get)
+    list_of_xmls <- list()
+    for(xml_name in all_xml_names){
+      list_of_xmls[[xml_name]] <- get(xml_name)
+    }
+    return_multiple_xmls(list_of_xmls, all_xml_names)
   } else {
     stop("Unknown command")
   }
